95874759cebc63a3b4264d08e0f523fe17e09199
unknown <jtc7537@gmail.com>
2018-02-23 21:17:03 -0500

    set database to update when items are purchased

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,10 +11,6 @@ var con = mysql.createConnection({
 
 con.connect(function(err){
     if (err) throw err;
-    con.query('SELECT * FROM products', function(err, result, fields){
-        if (err) throw err;
-        console.log(result)
-    })
 })
 
 var databaseQuery = {
@@ -33,7 +29,40 @@ var databaseQuery = {
     },
     purchaseQuantity: (id) => {
         console.log('second inquirer ran')
-    }
-}
+        inquirer.prompt([
+            {
+                message: 'How many items would you like to purchase?',
+                name: 'amount'
+            }
+        ]).then(function(response){
+            var num = response.amount;
+            console.log(response)
+            console.log(`Are you sure you would like to purchase ${num} of item ${id}?`)
+            databaseQuery.checkStock(id,num)
+        }).catch(function(err){
+            console.log(err)
+        })
+    },
+    checkStock: (id,amount) => {
+        con.query(`SELECT * FROM products WHERE item_id = ${id}`, function(err, result, fields){
+        if (err) throw err;
+        var remStock = result[0].stock_quantity;
+        if(remStock>amount){
+            console.log('There are enough items in stock to complete this order')
+            databaseQuery.buyItems(id,remStock,amount)
+        } else {
+            console.log('There are not enough items left in stock to complete this order')
+        }
+
+        });
+    },
+    buyItems: (id,remStock,amount) => {
+        var sql = `update products set stock_quantity = ${remStock - amount} where item_id = ${id}`;
+        con.query(sql, function(err,result){
+            if (err) throw err;
+            console.log(result.affectedRows + 'Records updated')
+        });
+    },
+};
 
 databaseQuery.purchaseId()
